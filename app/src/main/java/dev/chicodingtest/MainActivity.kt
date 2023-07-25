8e0477437a01c954e74dd6793da4829be958d413
PasararAndrey <pasararandrey@gmail.com>
2023-07-25 18:54:09 +0300

    middle level completed

--- app/src/main/java/dev/chicodingtest/MainActivity.kt
+++ app/src/main/java/dev/chicodingtest/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.chicodingtest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dev.chicodingtest.databinding.ActivityMainBinding
 
@@ -16,15 +17,38 @@ class MainActivity : AppCompatActivity() {
         setupUI()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COUNTER_STATE, counter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        counter = savedInstanceState.getInt(COUNTER_STATE, 0)
+        Log.d(TAG, "Restored value of counter:$counter")
+        setCounter()
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
     private fun setupUI() {
         binding.tvCounter.text = "0"
         binding.btnIncrement.setOnClickListener {
-            incrementCounter()
+            incrementAndSetCounter()
         }
     }
 
-    private fun incrementCounter() {
+    private fun incrementAndSetCounter() {
         counter++
+        Log.d(TAG, "Counter value:$counter")
+        setCounter()
+    }
+
+    private fun setCounter() {
         binding.tvCounter.text = counter.toString()
+    }
+
+
+    companion object {
+        private const val COUNTER_STATE = "counter_state"
+        private const val TAG = "main_activity"
     }
 }
